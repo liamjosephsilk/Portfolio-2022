b5ae1374a6044b78b993b40a4aeb1b39894d5a3c
Liam Joseph Silk <liamjosephsilk@gmail.com>
2022-01-10 21:51:58 +0000

    tailwind base

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.css';
+	import "@fontsource/inter";
+	import "cal-sans";
 </script>
+<div class="px-2 md:px-24 lg:px-80 text-white bg-[#222222]">
+	<main class="">
+		<slot />
+	</main>
+</div>
 
-<slot />
